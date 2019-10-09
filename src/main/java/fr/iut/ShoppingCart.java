7bfb1176b58fbc8bf683e5849411a3a14ce119fe
GUILLET Maxime <gm801652@iutnice.unice.fr>
2019-10-09 14:54:03 +0200

    tests addItem

--- src/main/java/fr/iut/ShoppingCart.java
+++ src/main/java/fr/iut/ShoppingCart.java
@@ -5,9 +5,15 @@ import java.util.Map;
 
 public class ShoppingCart {
 
-    private Map<Item, Integer> cart = new HashMap<Item, Integer>();
+    private Map<Item, Integer> content = new HashMap<Item, Integer>();
 
     public void addItem(final Item item, final int quantity) {
-        cart.put(item, quantity);
+        content.put(item, quantity);
+    }
+
+    public Map<Item, Integer> getContent() {
+        final Map<Item, Integer> copied = new HashMap<Item, Integer>();
+        copied.putAll(content);
+        return copied;
     }
 }
--- src/test/java/fr/iut/TestShoppingCart.java
+++ src/test/java/fr/iut/TestShoppingCart.java
@@ -1,6 +1,9 @@
 package fr.iut;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 public class TestShoppingCart {
 
@@ -9,6 +12,10 @@ public class TestShoppingCart {
         final ShoppingCart shoppingCart = new ShoppingCart();
         final Item carrot = new Item("carrot", 2.5);
         shoppingCart.addItem(carrot, 1);
+
+        Assert.assertEquals(shoppingCart.getContent(), new HashMap<Item, Integer>() {{
+            put(carrot, 1);
+        }});
     }
 
 }
